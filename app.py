fa17270dc58cc405396f5c0c627a5e15b5d96789
kavkazec <kanstantsin.korkuts@instinctools.com>
2019-02-18 15:45:51 +0300

    Added data processor

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import click
 
-from main.translater import YandexDictionary
+from main.translater import YandexDictionary, parse_word_definitions
 
 
 @click.command()
@@ -8,6 +8,11 @@ from main.translater import YandexDictionary
 def translate(path_to_file):
     with open(path_to_file) as file:
         dictionary = YandexDictionary()
-        result = dictionary.translate([line.strip()
-                                      for line in file.readlines()
-                                      if line != ''])
+        translated_data = dictionary.translate([line.strip()
+                                                for line in file.readlines()
+                                                if line != ''])
+        processed_data = (parse_word_definitions(data) for data in translated_data)
+
+
+if __name__ == '__main__':
+    translate()
--- main/translater.py
+++ main/translater.py
@@ -1,10 +1,10 @@
 import os
 import json
 import requests
+import itertools
 
 
 class YandexDictionaryException(Exception):
-
     """
     Default YandexDictionary exception
     """
@@ -16,16 +16,16 @@ class YandexDictionaryException(Exception):
         413: "ERR_TEXT_TOO_LONG",
         422: "ERR_UNPROCESSABLE_TEXT",
         501: "ERR_LANG_NOT_SUPPORTED",
-        503: "ERR_SERVICE_NOT_AVAIBLE",
+        503: "ERR_SERVICE_NOT_AVAILABLE",
     }
 
-    def __init__(self, status_code, *args, **kwargs):
+    def __init__(self, status_code, *args):
         super(YandexDictionaryException, self).__init__(
-            self.error_codes.get(status_code), *args, **kwargs)
+            self.error_codes.get(status_code), *args)
 
 
 class YandexDictionary:
-    api_url = "https://dictionary.yandex.net/api/"\
+    api_url = "https://dictionary.yandex.net/api/" \
               "{version}/dicservice.json/{endpoint}"
     YANDEX_DICTIONARY_API_KEY = os.getenv('YANDEX_DICTIONARY_API_KEY')
 
@@ -34,7 +34,7 @@ class YandexDictionary:
             raise ValueError('Yandex dictionay key is not set into env. ')
         self.api_version = api_version or 'v1'
         self.api_endpoints = api_endpoints or \
-            {"langs": "getLangs", "lookup": "lookup"}
+                             {"langs": "getLangs", "lookup": "lookup"}
 
     def make_url(self, endpoint):
         return self.api_url.format(version=self.api_version,
@@ -44,7 +44,7 @@ class YandexDictionary:
         response = requests.get(self.make_url(self.api_endpoints['langs']),
                                 params={'key': self.YANDEX_DICTIONARY_API_KEY})
         if response.status_code != 200:
-            raise YandexDictionaryException(status_code)
+            raise YandexDictionaryException(response.status_code)
         return languages in json.loads(response.text)
 
     def translate(self, words, lang='en-ru'):
@@ -67,11 +67,11 @@ class YandexDictionary:
 
 
 def parse_word_definitions(word_definitions):
-    text = word_definition['text']
+    return itertools.groupby((word_definition_mapper(definition) for definition in word_definitions), lambda x: x[0])
 
 
 def word_definition_mapper(word_definition):
     text = word_definition['text']
-    transcription = word_definition['ts']
-    values = ((value['pos'], value['text']) for value in word_definition['tr'])
-    return (text, transcription, values)
+    transcription = word_definition['ts'] if 'ts' in word_definition else None
+    values = [(value['pos'], value['text']) for value in word_definition['tr']] if 'tr' in word_definition else None
+    return (text, transcription), values
